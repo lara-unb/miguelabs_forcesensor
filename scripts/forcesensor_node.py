0f099e2a8a3335b87640eea7c6d936f2afffeeac
Lucas de Macêdo Pinheiro <lucasmp10@hotmail.com>
2020-04-05 12:26:35 -0300

    Headers hopefully to be helpful and other minor changes

--- scripts/forcesensor_node.py
+++ scripts/forcesensor_node.py
@@ -1,37 +1,52 @@
 #!/usr/bin/env python
 
+"""
+
+Particularly, this code initializes the force sensor device, receives its
+measurements and publish them as ROS messages.
+
+The ROS node runs this code. It should make all the necessary
+communication/interaction with ROS and it shouldn't deal with minor details.
+For example, it would be used to publish a filtered sensor measurement as
+a ROS message to other ROS nodes instead of establishing the serial comm
+and treating that raw measurement. For more info, check:
+http://wiki.ros.org/Nodes
+
+"""
+
 import rospy
-
-# import ros msgs
-from geometry_msgs.msg import WrenchStamped
-from std_msgs.msg import Float64
-
 import ema.modules.forcesensor as forcesensor
 
+# Import ROS msgs
+from std_msgs.msg import Float64
+from geometry_msgs.msg import WrenchStamped
+
+
 def main():
-    # init forcesensor node
+    # Init forcesensor node
     rospy.init_node('forcesensor', anonymous=False)
 
-    # get forcesensor config
+    # Get forcesensor config
     fs_manager = forcesensor.ForceSensor(rospy.get_param('/ema/forcesensor'))
 
-    # list published topics
+    # List published topics
     pub = {}
     for name in fs_manager.sensors:
         pub[name] = rospy.Publisher('forcesensor/' + name, WrenchStamped, queue_size=10)
-        pub[name + '_signal'] = rospy.Publisher('forcesensor/' + name + '_fsignal', Float64, queue_size=10)
+        pub[name + '_signal'] = rospy.Publisher('forcesensor/' + name + '_fsignal', 
+                                                 Float64, queue_size=10)
 
-    # define loop rate (in hz)
-    rate = rospy.Rate(100)
+    # Define loop rate (in hz)
+    rate = rospy.Rate(20)
 
-    # node loop
+    # Node loop
     while not rospy.is_shutdown():
 
         try:
             timestamp = rospy.Time.now()
             frame_id = 'base_link'
 
-            ## messages are shared by all force sensors
+            # Messages are shared by all force sensors
             forceMsg = WrenchStamped()
             forceMsg.header.stamp = timestamp
             forceMsg.header.frame_id = frame_id
@@ -55,10 +70,10 @@ def main():
         except TypeError:
             print 'TypeError occured!'
 
-        # sleep until it's time to work again
+        # Sleep until it's time to work again
         rate.sleep()
         
-    # cleanup
+    # Cleanup the communication
     fs_manager.terminate()
 
 if __name__ == '__main__':
